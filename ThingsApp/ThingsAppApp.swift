a35459ba8480433c94c28e945d86a9e81961b89b
Naveen <codingPresent@gmail.com>
2022-07-29 18:52:47 +0530

    Initial Commit

---
+++ ThingsApp/ThingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThingsAppApp.swift
+//  ThingsApp
+//
+//  Created by Naveen Kumawat on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ThingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
